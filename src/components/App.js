1679276532ab94abf612a3f4269bf8c2f342e73c
Esin <nbusra.esin@gmail.com>
2023-06-11 23:07:46 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React from "react";
 import ColorList from "./ColorList";
 
 function App() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
   return (
     <div>
-      <ColorList />
+      <ColorList esin = {colors} />
     </div>
   );
 }
